19f72b472b5820fa0985eca9f632398f9eb3817a
james <gurtowsk@cshl.edu>
2012-10-25 12:42:47 -0400

    changed to custom build system

---
+++ build.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+
+THIS_SCRIPT = os.path.dirname(os.path.realpath( __file__ ))
+
+PATHS = {
+    'SRC': "src/main/java",
+    'OUT': "target",
+    'LIB': "lib",
+    'CLASSPATH': '',
+    'OUT_JAR': 'jnomics-0.3.jar'
+}
+
+MAIN_CLASS = 'edu/cshl/schatz/jnomics/tools/JnomicsMain'
+
+
+def compile():
+    for p in PATHS.iterkeys():
+        PATHS[p] = os.path.join(THIS_SCRIPT, PATHS[p])
+
+    LIBRARIES = filter(lambda x: x.endswith(".jar"), os.listdir(PATHS['LIB']))
+
+    CLASSPATH = ":".join(map(lambda x: os.path.join(PATHS['LIB'], x), LIBRARIES))
+    
+    SRC_FILE_TREE = []
+
+    def vis(files,dirname,names):
+        java_files = filter(lambda x: os.path.isfile(os.path.join(dirname,x)) and x.endswith(".java"), names)
+        files += map(lambda x: os.path.join(dirname,x),java_files)
+        return names
+
+    os.path.walk(PATHS['SRC'], vis,SRC_FILE_TREE)
+
+    SRC_FILES = " ".join( SRC_FILE_TREE ) 
+
+    if not os.path.exists(PATHS['OUT']):
+        os.mkdir(PATHS['OUT'])
+
+    cmd = "javac -d %s -cp %s %s" % (PATHS['OUT'], CLASSPATH, SRC_FILES)
+    print "Compiling %d files into %s" % (len(SRC_FILE_TREE), PATHS['OUT'])
+    os.system(cmd)
+
+
+def jar():
+    if not os.path.exists(PATHS['OUT']):
+        compile()
+
+    out_jar = PATHS['OUT_JAR']
+    if os.path.exists(out_jar):
+        print "Found old jar: %s ... removing" % out_jar
+        os.remove(out_jar)
+
+    out_dir = PATHS['OUT']
+        
+    cmd = "jar -cfe %s %s -C %s ."  % (out_jar,MAIN_CLASS,out_dir)
+
+    print "Building Jar %s " % out_jar
+    os.system(cmd)
+
+
+def clean():
+    p = PATHS['OUT']
+    print "Removing: %s " % p
+    os.system('rm -rf %s' % p )
+
+    print "Removing %s " % PATHS['OUT_JAR']
+    os.system('rm %s '  % PATHS['OUT_JAR'] )
+
+TASKS = { 'compile' : compile,
+          'clean' : clean,
+          'help' : help,
+          'jar' : jar
+          }
+
+
+
+def help():
+    print "Available Tasks:"
+    for i in TASKS.iterkeys():
+        print i
+    
+if __name__ == "__main__":
+
+    if not len(sys.argv) > 1:
+        help();sys.exit()
+
+    task = sys.argv[1]
+
+    if not TASKS.has_key(task):
+        print "UNKNOWN TASK: %s" % task
+        print help()
+        sys.exit()
+    else:
+        TASKS[task]()
+    
+
+
